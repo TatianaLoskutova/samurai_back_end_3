31133424261368ab2f9ca5b60d1d578a8a049cee
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-01 14:43:36 +0300

    before express libriary

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const http = require('http')
-const fs = require('fs')
+const fs = require('fs') // чтобы прога читала другой файл, подкулючаем модул fs = file system
 
-const server = http.createServer(async (request, response)=> {
-    switch (request.url){
-        case '/home':...
-        case '/about':...
-        case '/api tables':...
-        case '/api users':...
-        case '/api account':...
-        case '/api comment':...
-        case '/api feedback':...{
-            response.statusCode = 400
-            response.setHeader('Content-type', 'application/json');
-            response.end(JSON.stringify({error: 'incorrect text length'}));
+const server = http.createServer(async (request, response) => {
+    switch (request.url) {
+        case '/home': {
+            try {
+                const data = await readFile('pages/about.html')
+                response.write(data)
+                response.end()
+            } catch (err) {
+                response.write('something wrong, 500')
+                response.end()
+            }
+            break;
+        }  // (err -если все плохо будет ошибка, если ок, то data)
+        case '/about': {
+            await delay(3000)
+            response.write('About course')
+            response.end()
             break;
         }
         default: {
-            //response.write('404 not found')
+            response.write('404 not found')
             response.end()
         }
     }
 })
+
 server.listen(3003)
